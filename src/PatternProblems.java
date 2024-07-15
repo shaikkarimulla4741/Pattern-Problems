3664a1fa042f3a5d2c43bd64ac466604007ab288
Nhanna <karimullabuzzshaik@gmail.com>
2024-07-15 21:33:12 +0530

    Solved Character Problems

--- src/PatternProblems.java
+++ src/PatternProblems.java
@@ -1,8 +1,102 @@
 public class PatternProblems {
 
     public static void main(String[] args) {
+        charPattern1(5);
         starPattern1(5);
     }
+
+    static void charPattern8(int n) {
+        for (int row = 1, c = 'E'; row <= n; row++, c--) {
+            int p = c;
+            for (int col = 0; col <= row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = row; col <= n; col++, p--) {
+
+                System.out.print((char) p);
+            }
+            System.out.println();
+        }
+    }
+
+    static void charPattern7(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = row, p = 'A'; col <= n; col++, p++) {
+
+                System.out.print((char) p + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void charPattern6(int n) {
+        for (int row = 0; row <=  n; row++) {
+            for (int col = 0, p = 'A'; col <= row; col++, p++) {
+                System.out.print((char) p + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void charPattern5(int n) {
+        for (int row = 0, p = 'A'; row <= 2 * n; row++, p++) {
+            int loopCol = row > n ? 2 * n - row : row;
+            int noOfSpaces = n - loopCol;
+
+            for (int col = 0; col <= noOfSpaces; col++) {
+                System.out.print(" ");
+
+            }
+            for (int col = 0; col <= loopCol; col++) {
+                System.out.print((char) p + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void charPattern4(int n) {
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= row; col++) {
+                if (row % 2 == 0) {
+                    System.out.print("B");
+                }else {
+                    System.out.print("A");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void charPattern3(int n) {
+        for (int row = 0, p = 'A'; row <= n; row++, p = p + 2) {
+            for (int col = 0; col <= row; col++) {
+                System.out.print((char) p);
+            }
+            System.out.println();
+        }
+    }
+
+    static void charPattern2(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0, p = 'E'; col <= row; col++, p--) {
+                System.out.print((char) p);
+            }
+            System.out.println();
+        }
+    }
+
+    static void charPattern1(int n) {
+        for (int row = 0, p = 'A'; row <= n; row++, p++) {
+            for (int col = 0; col <= row; col++) {
+                System.out.print((char) p);
+            }
+            System.out.println();
+        }
+    }
+
 
     static void starPattern6(int n) {
         for (int row = 0; row <= n; row++) {
